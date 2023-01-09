9c6a13335c908a4e99b3aa0fb5d52a1979f35a6c
Yor-Jihons <ryo.malon814@gmail.com>
2023-01-09 11:29:24 +0900

    完了

--- from_20230106/計算命令/src/main.cpp
+++ from_20230106/計算命令/src/main.cpp
@@ -4,7 +4,7 @@
 #include<algorithm>
 #include<memory>
 #include<limits>
-#include<stack>
+#include<list>
 
 using namespace std;
 
@@ -46,8 +46,139 @@ namespace Util{
     }
 }
 
+namespace Original{
+    class IProcess{
+        public:
+            IProcess(){}
+            virtual ~IProcess(){}
+
+            virtual double Calc( double n ) = 0;
+
+            virtual std::string ToString( void ) const = 0;
+    };
+
+    class AdditionProcess : public IProcess{
+        public:
+            AdditionProcess( int a ) : a_(a){}
+            ~AdditionProcess(){}
+
+            virtual double Calc( double n ) override{
+                return n + this->a_;
+            }
+
+            virtual std::string ToString( void ) const override{
+                return "Add " + std::to_string( this->a_ );
+            }
+        private:
+            int a_;
+    };
+
+    class SubtractionProcess : public IProcess{
+        public:
+            SubtractionProcess( int a ) : a_(a){}
+            ~SubtractionProcess(){}
+
+            virtual double Calc( double n ) override{
+                return n - this->a_;
+            }
+
+            virtual std::string ToString( void ) const override{
+                return "Sub " + std::to_string( this->a_ );
+            }
+        private:
+            int a_;
+    };
+
+    class MultiplicationProcess : public IProcess{
+        public:
+            MultiplicationProcess( int a ) : a_(a){}
+            ~MultiplicationProcess(){}
+
+            virtual double Calc( double n ) override{
+                return n * this->a_;
+            }
+
+            virtual std::string ToString( void ) const override{
+                return "Multiply " + std::to_string( this->a_ );
+            }
+        private:
+            int a_;
+    };
+
+    class DivisionProcess : public IProcess{
+        public:
+            DivisionProcess( int a ) : a_(a){}
+            ~DivisionProcess(){}
+
+            virtual double Calc( double n ) override{
+                return n / this->a_;
+            }
+
+            virtual std::string ToString( void ) const override{
+                return "Divide " + std::to_string( this->a_ );
+            }
+        private:
+            int a_;
+    };
+
+    Original::IProcess* CreateProcessObject( const std::string& str ){
+        Util::StrVec sv = Util::split( str );
+
+        if( sv[0].compare( "add" ) == 0 ) return new AdditionProcess( std::atoi( sv[1].c_str() ) );
+        if( sv[0].compare( "sub" ) == 0 ) return new SubtractionProcess( std::atoi( sv[1].c_str() ) );
+        if( sv[0].compare( "multiply" ) == 0 ) return new MultiplicationProcess( std::atoi( sv[1].c_str() ) );
+        if( sv[0].compare( "divide" ) == 0 ) return new DivisionProcess( std::atoi( sv[1].c_str() ) );
+    return nullptr;
+    }
+
+    class ProcessManager{
+        public:
+            ProcessManager(){
+
+            }
+
+            ~ProcessManager(){
+                for( int i = 0; i < processes_.size(); i++ ){
+                    delete processes_[i];
+                }
+            }
+
+            void Add( Original::IProcess* proc ){
+                processes_.push_back( proc );
+            }
+
+            double Calc( double n ){
+                double target = n;
+                for( int i = 0; i < processes_.size(); i++ ){
+                    target = processes_[i]->Calc( target );
+                }
+            return target;
+            }
+        private:
+            std::vector<IProcess*> processes_;
+    };
+}
+
 
 int main( int argc, char** argv ){
-    
+    double n;
+    int m;
+    {
+        std::string s;
+        std::getline( cin, s );
+        Util::StrVec sv = Util::split( s );
+        n = std::stod( sv[0].c_str() );
+        m = std::atoi( sv[1].c_str() );
+    }
+
+    Original::ProcessManager processManager;
+    for( int i = 0; i < m; i++ ){
+        std::string str;
+        std::getline( cin, str );
+        processManager.Add( Original::CreateProcessObject( str ) );
+    }
+
+    cout << processManager.Calc( n ) << endl;
+
 return 0;
 }
